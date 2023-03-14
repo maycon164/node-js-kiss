1d8601daaf211de9dbccae79edd304e5613e9222
maycon <mayconfelipe164@gmail.com>
2023-03-14 14:16:35 -0300

    redis session managment

---
+++ oauth-google-login/services/RedisStoreSession.js
@@ -0,0 +1,17 @@
+const RedisStore = require('connect-redis').default;
+const { createClient } = require('redis');
+
+const redisClient = createClient({
+    host: '127.0.0.1',
+    port: '6379'
+});
+
+redisClient.connect().catch(err => console.error(err));
+
+const redisStore = new RedisStore({
+    client: redisClient,
+    prefix: 'app',
+    disableTouch: true
+});
+
+module.exports = { redisStore }
